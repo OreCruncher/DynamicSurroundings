82b8d21533b5dc1969effeafb31cd5aec626e76a
OreCruncher <tedjkaiser@gmail.com>
2017-01-24 18:22:19 -0800

    Footprints!

--- src/main/java/org/blockartistry/mod/DynSurround/client/footsteps/system/Association.java
+++ src/main/java/org/blockartistry/mod/DynSurround/client/footsteps/system/Association.java
@@ -24,25 +24,41 @@
 
 package org.blockartistry.mod.DynSurround.client.footsteps.system;
 
+import java.util.Set;
+
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import org.blockartistry.mod.DynSurround.client.footsteps.implem.AcousticsManager;
 import org.blockartistry.mod.DynSurround.client.footsteps.interfaces.IAcoustic;
 import org.blockartistry.mod.DynSurround.util.MCHelper;
 import org.blockartistry.mod.DynSurround.util.MyUtils;
 
+import gnu.trove.set.hash.THashSet;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class Association {
+	
+	private static final Set<Material> FOOTPRINTABLE = new THashSet<Material>();
+	static {
+		FOOTPRINTABLE.add(Material.CLAY);
+		FOOTPRINTABLE.add(Material.GRASS);
+		FOOTPRINTABLE.add(Material.GROUND);
+		FOOTPRINTABLE.add(Material.ICE);
+		FOOTPRINTABLE.add(Material.SAND);
+	}
 
 	private final IBlockState state;
 	private final BlockPos pos;
 	private IAcoustic[] data;
+	private Vec3d stepLoc;
 
 	public Association() {
 		this(AcousticsManager.EMPTY);
@@ -76,6 +92,15 @@ public class Association {
 	public boolean isLiquid() {
 		return this.state != null && this.state.getMaterial().isLiquid();
 	}
+	
+	public boolean hasFootstepImprint() {
+		return this.state != null && FOOTPRINTABLE.contains(this.state.getMaterial());
+	}
+	
+	@Nullable
+	public Vec3d getStepLocation() {
+		return this.stepLoc;
+	}
 
 	public SoundType getSoundType() {
 		return this.state != null ? MCHelper.getSoundType(this.state) : null;
@@ -83,6 +108,10 @@ public class Association {
 
 	public void add(@Nonnull final IAcoustic acoustics) {
 		this.data = MyUtils.append(this.data, acoustics);
+	}
+	
+	public void setStepLocation(@Nonnull final Vec3d stepLoc) {
+		this.stepLoc = stepLoc;
 	}
 
 	@Nonnull
