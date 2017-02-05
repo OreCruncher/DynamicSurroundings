c127ce10d3771464bb53f427bbce3a9e8b731137
OreCruncher <tedjkaiser@gmail.com>
2017-02-04 20:39:14 -0800

    LightLevel tweaks

--- src/main/java/org/blockartistry/mod/DynSurround/client/hud/LightLevelHUD.java
+++ src/main/java/org/blockartistry/mod/DynSurround/client/hud/LightLevelHUD.java
@@ -24,22 +24,23 @@
 
 package org.blockartistry.mod.DynSurround.client.hud;
 
-import java.util.Arrays;
-import java.util.BitSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.Nonnull;
 
 import org.blockartistry.mod.DynSurround.client.handlers.EnvironStateHandler.EnvironState;
 import org.blockartistry.mod.DynSurround.util.Color;
-import net.minecraft.block.state.IBlockState;
+
+import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.EnumSkyBlock;
-import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -49,13 +50,29 @@ import net.minecraftforge.fml.relauncher.Side;
 public final class LightLevelHUD {
 
 	public static enum Mode {
-		NONE, BLOCK_SKY, BLOCK;
+		NONE, BLOCK, BLOCK_SKY;
 
 		public static Mode cycle(final Mode mode) {
 			int next = mode.ordinal() + 1;
 			if (next >= values().length)
 				next = 0;
 			return values()[next];
+		}
+	}
+
+	private static class LightCoord {
+		public final int x;
+		public final int y;
+		public final int z;
+		public final int lightLevel;
+		public String text;
+
+		public LightCoord(final int x, final int y, final int z, final int light) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.lightLevel = light;
+			this.text = Integer.toString(light);
 		}
 	}
 
@@ -66,7 +83,6 @@ public final class LightLevelHUD {
 
 	private static final int DIM_XZ = 24 * 2 + 1;
 	private static final int DIM_Y = 24 + 1;
-	private static final int VOLUME = DIM_XZ * DIM_XZ * DIM_Y;
 
 	private static final int X_OFFSET = DIM_XZ / 2;
 	private static final int Z_OFFSET = DIM_XZ / 2;
@@ -75,66 +91,62 @@ public final class LightLevelHUD {
 	private static final int NO_SPAWN = Color.MC_GREEN.rgbWithAlpha(0.75F);
 	private static final int SPAWN = Color.MC_RED.rgbWithAlpha(0.75F);
 
-	private static final byte[] lightLevels = new byte[VOLUME];
-	private static final BitSet blockMobSpawn = new BitSet(VOLUME);
-	private static BlockPos origin = BlockPos.ORIGIN;
-
-	protected static int calculateIndex(final int x, final int y, final int z) {
-		return y + DIM_Y * (z + DIM_XZ * x);
-	}
+	private static List<LightCoord> lightLevels = new ArrayList<LightCoord>();
 
 	protected static void updateLightInfo() {
 		final long tick = EnvironState.getTickCounter();
 		if (tick == 0 || tick % 4 != 0)
 			return;
 
-		// Reset our data for the calculation pass
-		Arrays.fill(lightLevels, (byte) -1);
-		blockMobSpawn.clear();
+		final BlockPos origin = EnvironState.getPlayerPosition();
+		lightLevels = new ArrayList<LightCoord>();
 
-		final World world = EnvironState.getWorld();
-		origin = EnvironState.getPlayerPosition();
-		final int skyLightSub = world.calculateSkylightSubtracted(1.0F);
+		final int skyLightSub = EnvironState.getWorld().calculateSkylightSubtracted(1.0F);
+		final IChunkProvider provider = EnvironState.getWorld().getChunkProvider();
 		final BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
+
+		Chunk chunk = null;
 
 		for (int dX = 0; dX < DIM_XZ; dX++)
 			for (int dZ = 0; dZ < DIM_XZ; dZ++) {
 
-				final int trueX = origin.getX() + dX - X_OFFSET;
 				final int trueZ = origin.getZ() + dZ - Z_OFFSET;
+				final int trueX = origin.getX() + dX - X_OFFSET;
 
-				final Chunk chunk = world.getChunkFromChunkCoords(trueX >> 4, trueZ >> 4);
-				if (!chunk.isLoaded())
+				final int chunkX = trueX >> 4;
+				final int chunkZ = trueZ >> 4;
+
+				if (chunk == null || chunk.xPosition != chunkX || chunk.zPosition != chunkZ)
+					chunk = provider.getLoadedChunk(chunkX, chunkZ);
+
+				if (chunk == null)
 					return;
 
-				IBlockState lastState = null;
-				BlockPos lastPos = null;
+				Material lastMaterial = null;
+
 				for (int dY = 0; dY < DIM_Y; dY++) {
 
 					final int trueY = origin.getY() + dY - Y_OFFSET;
 					if (trueY < 1)
 						continue;
 
-					mutable.setPos(trueX, trueY, trueZ);
-					IBlockState currentState = chunk.getBlockState(mutable);
-					if (!currentState.getMaterial().isSolid() && !currentState.getMaterial().isLiquid()) {
+					final Material currentMaterial = chunk.getBlockState(trueX, trueY, trueZ).getMaterial();
+					if (!currentMaterial.isSolid() && !currentMaterial.isLiquid()) {
 
-						if (lastState == null) {
-							lastPos = mutable.down();
-							lastState = world.getBlockState(lastPos);
+						if (lastMaterial == null) {
+							lastMaterial = chunk.getBlockState(trueX, trueY - 1, trueZ).getMaterial();
 						}
 
-						if (lastState.getMaterial().isSolid()) {
-							final int index = calculateIndex(dX, dY, dZ);
+						if (lastMaterial.isSolid()) {
+							mutable.setPos(trueX, trueY, trueZ);
 							final int blockLight = chunk.getLightFor(EnumSkyBlock.BLOCK, mutable);
 							final int skyLight = displayMode == Mode.BLOCK_SKY
 									? chunk.getLightFor(EnumSkyBlock.SKY, mutable) - skyLightSub : 0;
-							lightLevels[index] = (byte) Math.max(blockLight, skyLight);
+							lightLevels.add(new LightCoord(trueX, trueY, trueZ, Math.max(blockLight, skyLight)));
 						}
 					}
 
-					lastState = currentState;
-					lastPos = mutable.toImmutable();
+					lastMaterial = currentMaterial;
 				}
 			}
 
@@ -148,17 +160,13 @@ public final class LightLevelHUD {
 
 		// Update state if needed
 		updateLightInfo();
-		if (origin == BlockPos.ORIGIN)
+
+		if (lightLevels.size() == 0)
 			return;
 
 		// Render the data.
 		final RenderManager manager = Minecraft.getMinecraft().getRenderManager();
-		if (manager == null || manager.options == null)
-			return;
-
 		final FontRenderer font = Minecraft.getMinecraft().fontRendererObj;
-		if (font == null)
-			return;
 
 		GlStateManager.pushMatrix();
 		GlStateManager.pushAttrib();
@@ -171,43 +179,29 @@ public final class LightLevelHUD {
 		GlStateManager.enableDepth();
 		GlStateManager.depthMask(true);
 
-		for (int dX = 0; dX < DIM_XZ; dX++)
-			for (int dZ = 0; dZ < DIM_XZ; dZ++)
-				for (int dY = 0; dY < DIM_Y; dY++) {
+		final boolean thirdPerson = manager.options.thirdPersonView == 2;
+		final float pitch = manager.playerViewX * (thirdPerson ? -1 : 1);
+		final float yaw = -manager.playerViewY;
 
-					final int index = calculateIndex(dX, dY, dZ);
-					final byte light = lightLevels[index];
-					if (light < 0)
-						continue;
+		for (final LightCoord coord : lightLevels) {
+			final double x = coord.x - manager.viewerPosX + 0.5D;
+			final double y = coord.y - manager.viewerPosY + 0.3D;
+			final double z = coord.z - manager.viewerPosZ + 0.5D;
+			final int color = coord.lightLevel <= MOB_SPAWN_LEVEL ? SPAWN : NO_SPAWN;
+			final int margin = -font.getStringWidth(coord.text) / 2;
 
-					GlStateManager.pushMatrix();
-					GlStateManager.pushAttrib();
+			GlStateManager.pushMatrix();
+			GlStateManager.pushAttrib();
+			GlStateManager.translate(x, y, z);
+			GlStateManager.rotate(yaw, 0.0F, 1.0F, 0.0F);
+			GlStateManager.rotate(pitch, 1.0F, 0.0F, 0.0F);
+			GlStateManager.scale(-SCALE, -SCALE, SCALE);
 
-					final double x = origin.getX() - manager.viewerPosX + 0.5D + dX - X_OFFSET;
-					final double y = origin.getY() - manager.viewerPosY + 0.3D + dY - Y_OFFSET;
-					final double z = origin.getZ() - manager.viewerPosZ + 0.5D + dZ - Z_OFFSET;
+			font.drawString(coord.text, margin, 0, color);
 
-					GlStateManager.translate(x, y, z);
-
-					final boolean thirdPerson = manager.options.thirdPersonView == 2;
-					final float pitch = manager.playerViewX * (thirdPerson ? -1 : 1);
-					final float yaw = -manager.playerViewY;
-
-					GlStateManager.rotate(yaw, 0.0F, 1.0F, 0.0F);
-					GlStateManager.rotate(pitch, 1.0F, 0.0F, 0.0F);
-
-					GlStateManager.scale(-SCALE, -SCALE, SCALE);
-
-					final int color = light <= MOB_SPAWN_LEVEL ? SPAWN : NO_SPAWN;
-
-					final String text = Integer.toString(light);
-					final int margin = -font.getStringWidth(text) / 2;
-
-					font.drawString(text, margin, 0, color);
-
-					GlStateManager.popAttrib();
-					GlStateManager.popMatrix();
-				}
+			GlStateManager.popAttrib();
+			GlStateManager.popMatrix();
+		}
 
 		GlStateManager.popAttrib();
 		GlStateManager.popMatrix();
