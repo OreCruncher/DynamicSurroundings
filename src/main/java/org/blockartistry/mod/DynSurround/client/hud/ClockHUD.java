fe312a234e640dc396abaf4d0d6ddd2d58e7182a
OreCruncher <tedjkaiser@gmail.com>
2017-04-07 09:47:57 -0700

    Fancy clock hud

--- src/main/java/org/blockartistry/mod/DynSurround/client/hud/ClockHUD.java
+++ src/main/java/org/blockartistry/mod/DynSurround/client/hud/ClockHUD.java
@@ -29,7 +29,6 @@ import javax.annotation.Nonnull;
 import org.blockartistry.mod.DynSurround.DSurround;
 import org.blockartistry.mod.DynSurround.ModOptions;
 import org.blockartistry.mod.DynSurround.client.handlers.EnvironStateHandler.EnvironState;
-import org.blockartistry.mod.DynSurround.client.hud.GuiHUDHandler.GuiOverlay;
 import org.blockartistry.mod.DynSurround.util.Color;
 import org.blockartistry.mod.DynSurround.util.Localization;
 import org.blockartistry.mod.DynSurround.util.PlayerUtils;
@@ -47,6 +46,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class ClockHUD extends GuiOverlay {
 
+	private static final Color BACKGROUND_COLOR = Color.DARKSLATEGRAY;
+	private static final Color FRAME_COLOR = Color.MC_WHITE;
 	private static final Color TIME_COLOR = Color.MC_YELLOW;
 
 	private int elapsedMinutes;
@@ -95,10 +96,21 @@ public class ClockHUD extends GuiOverlay {
 		GlStateManager.color(1F, 1F, 1F, ModOptions.compassTransparency);
 		GlStateManager.enableBlend();
 
-		drawCenteredString(font, time, centerX, (int) (centerY),
-				TIME_COLOR.rgbWithAlpha(ModOptions.compassTransparency));
-		drawCenteredString(font, elapsed, centerX, (int) (centerY + 15),
-				TIME_COLOR.rgbWithAlpha(ModOptions.compassTransparency));
+		final int width = Math.max(font.getStringWidth(this.time), font.getStringWidth(this.elapsed)) + 10;
+		final int height = 2 * font.FONT_HEIGHT + 10;
+
+		final int x1 = centerX - (width+1) / 2;
+		final int y1 = centerY - (height+1) / 2;
+
+		final int backgroundRGB = BACKGROUND_COLOR.rgbWithAlpha(ModOptions.compassTransparency);
+		final int textRGB = TIME_COLOR.rgbWithAlpha(ModOptions.compassTransparency);
+		final int frameRGB = FRAME_COLOR.rgbWithAlpha(ModOptions.compassTransparency);
+
+		drawRect(x1 + 2, y1 + 2, x1 + width - 1, y1 + height - 1, backgroundRGB);
+		drawTooltipBox(x1, y1, width, height, frameRGB, frameRGB, frameRGB);
+
+		drawCenteredString(font, time, centerX, (int) (centerY) - font.FONT_HEIGHT, textRGB);
+		drawCenteredString(font, elapsed, centerX, (int) (centerY), textRGB);
 
 		GlStateManager.color(1F, 1F, 1F, 1F);
 
