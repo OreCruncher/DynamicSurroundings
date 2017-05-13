14f958c762db94517e1af88ab2beb2faf289b451
OreCruncher <tedjkaiser@gmail.com>
2017-05-13 09:15:20 -0700

    WeatherGenerators

--- src/main/java/org/blockartistry/DynSurround/server/services/AtmosphereService.java
+++ src/main/java/org/blockartistry/DynSurround/server/services/AtmosphereService.java
@@ -24,57 +24,24 @@
 
 package org.blockartistry.DynSurround.server.services;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.storage.WorldInfo;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import net.minecraftforge.fml.relauncher.Side;
 
-import java.util.Random;
 import javax.annotation.Nonnull;
 
-import org.blockartistry.DynSurround.DSurround;
 import org.blockartistry.DynSurround.ModEnvironment;
 import org.blockartistry.DynSurround.ModOptions;
-import org.blockartistry.DynSurround.data.DimensionEffectData;
-import org.blockartistry.DynSurround.network.Network;
-import org.blockartistry.DynSurround.network.PacketThunder;
-import org.blockartistry.DynSurround.network.PacketWeatherUpdate;
 import org.blockartistry.DynSurround.registry.DimensionRegistry;
 import org.blockartistry.DynSurround.registry.RegistryManager;
 import org.blockartistry.DynSurround.registry.RegistryManager.RegistryType;
-import org.blockartistry.lib.PlayerUtils;
-import org.blockartistry.lib.random.XorShiftRandom;
+import gnu.trove.map.hash.TIntObjectHashMap;
 
 public final class AtmosphereService extends Service {
-
-	private static final Random RANDOM = XorShiftRandom.current();
-
-	private static int nextThunderInterval(final boolean isThundering) {
-		final int base = isThundering ? ModOptions.stormActiveTimeConst : ModOptions.stormInactiveTimeConst;
-		return base + RANDOM
-				.nextInt(isThundering ? ModOptions.stormActiveTimeVariable : ModOptions.stormInactiveTimeVariable);
-	}
-
-	private static int nextRainInterval(final boolean isRaining) {
-		final int base = isRaining ? ModOptions.rainActiveTimeConst : ModOptions.rainInactiveTimeConst;
-		return base
-				+ RANDOM.nextInt(isRaining ? ModOptions.rainActiveTimeVariable : ModOptions.rainInactiveTimeVariable);
-	}
-
-	private static int nextThunderEvent(final float rainIntensity) {
-		final float scale = 2.0F - rainIntensity;
-		return RANDOM.nextInt((int) (450 * scale)) + 300;
-	}
-
-	private static boolean doFlash(final float rainIntensity) {
-		final int randee = (int) (rainIntensity * 100.0F);
-		return RANDOM.nextInt(150) <= randee;
-	}
 
 	private final DimensionRegistry dimensions = RegistryManager.get(RegistryType.DIMENSION);
 
@@ -89,113 +56,47 @@ public final class AtmosphereService extends Service {
 				|| ModEnvironment.Weather2.isLoaded())
 			return;
 
-		final World world = event.world;
-		final DimensionEffectData data = DimensionEffectData.get(world);
+		getGenerator(event.world).process();
+	}
 
-		if (!this.dimensions.hasWeather(world))
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public void onWorldLoad(final WorldEvent.Load e) {
+		final World world = e.getWorld();
+		if(world.isRemote)
+			return;
+		
+		final int dimId = world.provider.getDimension();
+		this.generators.put(dimId, createGenerator(world));
+	}
+
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public void onWorldLoad(final WorldEvent.Unload e) {
+		final World world = e.getWorld();
+		if(world.isRemote)
 			return;
 
-		// If we get here and the world has no sky we have a dimension
-		// like the Nether. We need to turn the crank manually to get
-		// Minecraft to do what we need.
-		if (world.provider.hasNoSky()) {
-			world.provider.hasNoSky = false;
-			try {
-				world.updateWeatherBody();
-			} catch (final Throwable t) {
-				;
-			}
-			world.provider.hasNoSky = true;
+		final int dimId = world.provider.getDimension();
+		this.generators.remove(dimId);
+	}
+
+	private final TIntObjectHashMap<WeatherGenerator> generators = new TIntObjectHashMap<WeatherGenerator>();
+
+	private WeatherGenerator createGenerator(@Nonnull final World world) {
+		WeatherGenerator result = WeatherGenerator.NONE;
+		if (this.dimensions.hasWeather(world)) {
+			if (world.provider.getDimension() == -1)
+				result = new WeatherGeneratorNether(world);
+			else
+				result = new WeatherGenerator(world);
 		}
 
-		final WorldInfo info = world.getWorldInfo();
+		return result;
+	}
 
-		// Tackle the rain and thunder timers. We toggle on remaining
-		// time of 2 since updateWeatherBody() triggers on 0. We
-		// want to control the timers.
-
-		final int rain = info.getRainTime();
-		if (rain == 2) {
-			info.setRaining(!info.isRaining());
-			info.setRainTime(nextRainInterval(info.isRaining()));
-		}
-
-		final int thunder = info.getThunderTime();
-		if (thunder == 2) {
-			info.setThundering(!info.isThundering());
-			info.setThunderTime(nextThunderInterval(info.isThundering()));
-		}
-
-		// Track our rain intensity values
-		if (info.isRaining()) {
-			// If our intensity is 0 it means that we need to establish
-			// a strength.
-			if (data.getRainIntensity() == 0.0F) {
-				data.randomizeRain();
-				info.setRainTime(nextRainInterval(true));
-				DSurround.log().debug("dim %d rain intensity set to %f, duration %d ticks", data.getDimensionId(),
-						data.getRainIntensity(), info.getRainTime());
-			}
-			data.setCurrentRainIntensity(world.getRainStrength(1.0F));
-		} else {
-
-			if (world.getRainStrength(1.0F) > 0.0F) {
-				// It's not raining and the world has strength. Means that it is
-				// on
-				// the way out so reflect the worlds strength.
-				data.setCurrentRainIntensity(world.getRainStrength(1.0F));
-			} else if (data.getCurrentRainIntensity() > 0) {
-				// We get here the world is not raining and there is no
-				// strength,
-				// but our record indicates something. Means we stopped.
-				data.setRainIntensity(0);
-				data.setCurrentRainIntensity(0);
-				info.setRainTime(nextRainInterval(false));
-				DSurround.log().debug("dim %d rain has stopped, next rain %d ticks", data.getDimensionId(),
-						world.getWorldInfo().getRainTime());
-			} else if (data.getRainIntensity() > 0) {
-				data.setRainIntensity(0);
-			}
-		}
-
-		// Do background thunder. For it to happen the world has to have a real
-		// sky, has to be enabled,
-		// has to be in the thunder window, and rain intensity must meet the
-		// threshold.
-		if (!world.provider.hasNoSky && ModOptions.allowBackgroundThunder && info.isThundering()
-				&& data.getCurrentRainIntensity() >= ModOptions.stormThunderThreshold) {
-
-			int time = data.getThunderTimer() - 1;
-			if (time <= 0) {
-				// If it is 0 we just counted down to this. If it were
-				// the first time through it would be -1.
-				if (time == 0) {
-					// Get a random player in the dimension - they will be the
-					// locus of the event. Center it at build height above
-					// their head.
-					final EntityPlayer player = PlayerUtils.getRandomPlayer(world);
-					final float theY = this.dimensions.getSkyHeight(world);
-					if (player != null) {
-						final PacketThunder packet = new PacketThunder(data.getDimensionId(),
-								doFlash(data.getCurrentRainIntensity()), new BlockPos(player.posX, theY, player.posZ));
-						Network.sendToDimension(data.getDimensionId(), packet);
-					}
-				}
-				// set new time
-				time = nextThunderEvent(data.getCurrentRainIntensity());
-			}
-			data.setThunderTimer(time);
-
-		} else {
-			// Clear out the timer data for the next storm
-			data.setThunderTimer(0);
-		}
-
-		// Send the weather update to all players in the dimension.
-		final PacketWeatherUpdate packet = new PacketWeatherUpdate(data.getDimensionId(),
-				data.getCurrentRainIntensity(), data.getRainIntensity(), info.getRainTime(),
-				world.getThunderStrength(1.0F), info.getThunderTime(), data.getThunderTimer());
-		Network.sendToDimension(data.getDimensionId(), packet);
+	private WeatherGenerator getGenerator(@Nonnull final World world) {
+		final int dimId = world.provider.getDimension();
+		WeatherGenerator result = this.generators.get(dimId);
+		return result == null ? WeatherGenerator.NONE : result;
 	}
 
 }
