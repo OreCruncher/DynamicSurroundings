5edfc81e4d73446e4fb76cbf4f7c36a05b45188c
OreCruncher <tedjkaiser@gmail.com>
2017-05-16 08:45:26 -0700

    Change id mappings for emoji states

--- src/main/java/org/blockartistry/DynSurround/api/entity/ActionState.java
+++ src/main/java/org/blockartistry/DynSurround/api/entity/ActionState.java
@@ -25,7 +25,7 @@
 package org.blockartistry.DynSurround.api.entity;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import gnu.trove.map.hash.TIntObjectHashMap;
 
 /**
  * Describes the current action that the entity is taking.
@@ -33,48 +33,69 @@ import javax.annotation.Nullable;
 public enum ActionState {
 
 	/** Not doing anything */
-	NONE(0, EmotionalState.NEUTRAL),
+	NONE(0, 0, EmotionalState.NEUTRAL),
 	/** Moving various model parts around, like shifting head orientation */
-	IDLE(1, EmotionalState.NEUTRAL),
-	/** Entity is angry and doing something related to that anger.  Enderman agitation is an example. */
-	ANGRY(2, EmotionalState.ANGRY),
+	IDLE(1, 1, EmotionalState.NEUTRAL),
+	/**
+	 * Entity is angry and doing something related to that anger. Enderman
+	 * agitation is an example.
+	 */
+	ANGRY(2, 2, EmotionalState.ANGRY),
 	/** The entity has a target and is attempting to attack. */
-	ATTACKING(1000, EmotionalState.ANGRY),
-	/** The entity is in the process of exploding.  Creeper swell is an example. */
-	EXPLODE(2000, EmotionalState.ANGRY),
+	ATTACKING(3, 1000, EmotionalState.ANGRY),
+	/**
+	 * The entity is in the process of exploding. Creeper swell is an example.
+	 */
+	EXPLODE(4, 2000, EmotionalState.ANGRY),
 	/** The entity is looking at something, either a player, mob, block, etc. */
-	LOOKING(3, EmotionalState.NEUTRAL),
+	LOOKING(5, 3, EmotionalState.NEUTRAL),
 	/** The villager is attempting to farm mature crops. */
-	FARMING(4, EmotionalState.BUSY),
+	FARMING(6, 4, EmotionalState.BUSY),
 	/** The entity is fleeing, such as a villager from a zombie. */
-	PANIC(5, EmotionalState.AFRAID),
-	/** The entity is being tempted.  Usually applies to passive mobs when player holds out food. */
-	TEMPT(6, EmotionalState.HAPPY),
+	PANIC(7, 5, EmotionalState.AFRAID),
+	/**
+	 * The entity is being tempted. Usually applies to passive mobs when player
+	 * holds out food.
+	 */
+	TEMPT(8, 6, EmotionalState.HAPPY),
 	/** Needs no explanation. */
-	MATING(7, EmotionalState.HAPPY),
+	MATING(9, 7, EmotionalState.HAPPY),
 	/** When a mob runs around like crazy. */
-	CRAZY(8, EmotionalState.AFRAID),
+	CRAZY(10, 8, EmotionalState.AFRAID),
 	/** Villager is engaged in a trade with a player. */
-	TRADING(9, EmotionalState.HAPPY),
+	TRADING(11, 9, EmotionalState.HAPPY),
 	/** Child villager is playing. */
-	PLAYING(10, EmotionalState.HAPPY),
+	PLAYING(12, 10, EmotionalState.HAPPY),
 	/** Pet is begging */
-	BEGGING(11, EmotionalState.HAPPY),
+	BEGGING(13, 11, EmotionalState.HAPPY),
 	/** Currently not used */
-	WORKING(12, EmotionalState.BUSY),
+	WORKING(14, 12, EmotionalState.BUSY),
 	/** The entity is eating, such as a sheep cropping grass. */
-	EATING(13, EmotionalState.HAPPY),
+	EATING(15, 13, EmotionalState.HAPPY),
 	/** The entity is following, like a leashed passive. */
-	FOLLOWING(14, EmotionalState.NEUTRAL),
+	FOLLOWING(16, 14, EmotionalState.NEUTRAL),
 	/** The entity is on the move. */
-	MOVING(15, EmotionalState.NEUTRAL);
+	MOVING(17, 15, EmotionalState.NEUTRAL);
 
+	private static final TIntObjectHashMap<ActionState> lookup = new TIntObjectHashMap<ActionState>();
+	static {
+		for (final ActionState state : ActionState.values()) {
+			lookup.put(state.getId(), state);
+		}
+	}
+
+	private final int id;
 	private final int priority;
 	private final EmotionalState state;
 
-	ActionState(final int priority, @Nonnull final EmotionalState state) {
+	ActionState(final int id, final int priority, @Nonnull final EmotionalState state) {
+		this.id = id;
 		this.priority = priority;
 		this.state = state;
+	}
+
+	public int getId() {
+		return this.id;
 	}
 
 	public int getPriority() {
@@ -86,16 +107,16 @@ public enum ActionState {
 		return this.state;
 	}
 
-	@Nullable
+	@Nonnull
 	public static ActionState get(int id) {
-		final ActionState[] v = ActionState.values();
-		if (id > v.length || id < 0)
-			return NONE;
-		return v[id];
+		ActionState result = lookup.get(id);
+		if (result == null)
+			result = ActionState.NONE;
+		return result;
 	}
 
 	public static int getId(@Nonnull final ActionState state) {
-		return state.ordinal();
+		return state.getId();
 	}
 
 }
--- src/main/java/org/blockartistry/DynSurround/api/entity/EmotionalState.java
+++ src/main/java/org/blockartistry/DynSurround/api/entity/EmotionalState.java
@@ -25,42 +25,59 @@
 package org.blockartistry.DynSurround.api.entity;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import gnu.trove.map.hash.TIntObjectHashMap;
 
 /**
- * Describes how an Entity "feels".  Though an ActionState has a default
- * EmotionalState, the EntityAI may select a different EmotionalState based
- * on other factors.
+ * Describes how an Entity "feels". Though an ActionState has a default
+ * EmotionalState, the EntityAI may select a different EmotionalState based on
+ * other factors.
  */
 public enum EmotionalState {
 
 	/** The Entity has no real opinion at the moment. */
-	NEUTRAL,
+	NEUTRAL(0),
 	/** The Entity is happy, like when being tempted with food. */
-	HAPPY,
+	HAPPY(1),
 	/** The entity is sad. */
-	SAD,
+	SAD(2),
 	/** The Entity is angry such as when attacking. */
-	ANGRY,
+	ANGRY(3),
 	/** The Entity is afraid, such as when fleeing. */
-	AFRAID,
+	AFRAID(4),
 	/** The Entity is busy with an activity, like Farming. */
-	BUSY,
+	BUSY(5),
 	/** The Entity feels sick because of a negative potion effect. */
-	SICK,
+	SICK(6),
 	/** The Entity feels hurt because it's health has been reduced. */
-	HURT;
+	HURT(7);
 
-	@Nullable
-	public static EmotionalState get(int id) {
-		final EmotionalState[] v = EmotionalState.values();
-		if (id > v.length || id < 0)
-			return NEUTRAL;
-		return v[id];
+	private static final TIntObjectHashMap<EmotionalState> lookup = new TIntObjectHashMap<EmotionalState>();
+	static {
+		for (final EmotionalState state : EmotionalState.values()) {
+			lookup.put(state.getId(), state);
+		}
 	}
 
-	public static int getId(@Nonnull final EmotionalState state) {
-		return state.ordinal();
+	private final int id;
+
+	EmotionalState(final int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	@Nonnull
+	public static EmotionalState get(int id) {
+		EmotionalState result = lookup.get(id);
+		if (result == null)
+			result = EmotionalState.NEUTRAL;
+		return result;
+	}
+
+	public static int getId(@Nonnull final EmotionalState type) {
+		return type.getId();
 	}
 
 }
