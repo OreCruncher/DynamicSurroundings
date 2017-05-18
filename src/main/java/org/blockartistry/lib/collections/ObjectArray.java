1d40b44414dbbba9dda5a06cd671465968095613
OreCruncher <tedjkaiser@gmail.com>
2017-05-17 18:57:28 -0700

    More tweaks

--- src/main/java/org/blockartistry/lib/collections/ObjectArray.java
+++ src/main/java/org/blockartistry/lib/collections/ObjectArray.java
@@ -23,11 +23,14 @@
 
 package org.blockartistry.lib.collections;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import javax.annotation.Nonnull;
 
 import com.google.common.base.Predicate;
 
-public class ObjectArray<T> {
+public class ObjectArray<T> implements Collection<T> {
 
 	private static final int DEFAULT_SIZE = 16;
 
@@ -48,16 +51,7 @@ public class ObjectArray<T> {
 		this.data = t;
 	}
 
-	public void add(@Nonnull final T e) {
-		if (e == null)
-			return;
-
-		if (this.data.length == this.insertionIdx)
-			resize();
-
-		this.data[this.insertionIdx++] = e;
-	}
-
+	@Override
 	public int size() {
 		return this.insertionIdx;
 	}
@@ -69,26 +63,125 @@ public class ObjectArray<T> {
 		return null;
 	}
 
-	@SuppressWarnings("unchecked")
-	public T remove(final int idx) {
-		T removed = null;
-		if (idx >= 0 && idx < this.insertionIdx) {
-			removed = (T) this.data[idx];
-			final Object m = this.data[--this.insertionIdx];
-			this.data[this.insertionIdx] = null;
-			if (idx != this.insertionIdx)
-				this.data[idx] = m;
-		}
-
+	private T remove0(final int idx) {
+		@SuppressWarnings("unchecked")
+		final T removed = (T) this.data[idx];
+		final Object m = this.data[--this.insertionIdx];
+		this.data[this.insertionIdx] = null;
+		if (idx != this.insertionIdx)
+			this.data[idx] = m;
 		return removed;
 	}
 
 	@SuppressWarnings("unchecked")
-	public void removeIf(@Nonnull final Predicate<T> pred) {
+	public boolean removeIf(@Nonnull final Predicate<T> pred) {
+		boolean result = false;
 		for (int i = this.insertionIdx - 1; i >= 0; i--) {
-			if (pred.apply((T) this.data[i]))
-				this.remove(i);
+			if (pred.apply((T) this.data[i])) {
+				result = true;
+				this.remove0(i);
+			}
 		}
+		return result;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.insertionIdx == 0;
+	}
+
+	private int find(@Nonnull final Object o) {
+		for (int i = 0; i < this.insertionIdx; i++)
+			if (this.data[i] == o)
+				return i;
+		return -1;
+	}
+
+	@Override
+	public boolean contains(@Nonnull final Object o) {
+		return find(o) != -1;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return new Iterator<T>() {
+
+			private int idx = -1;
+
+			@Override
+			public boolean hasNext() {
+				return (this.idx + 1) < ObjectArray.this.insertionIdx;
+			}
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public T next() {
+				return (T) ObjectArray.this.data[++this.idx];
+			}
+
+		};
+	}
+
+	@Override
+	public Object[] toArray() {
+		final Object[] result = new Object[this.insertionIdx];
+		System.arraycopy(this.data, 0, result, 0, this.insertionIdx);
+		return result;
+	}
+
+	@SuppressWarnings("hiding")
+	@Override
+	public <T> T[] toArray(T[] a) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean add(@Nonnull final T e) {
+		if (e == null)
+			return false;
+
+		if (this.data.length == this.insertionIdx)
+			resize();
+
+		this.data[this.insertionIdx++] = e;
+		return true;
+	}
+
+	@Override
+	public boolean remove(@Nonnull final Object o) {
+		final int idx = find(o);
+		if (idx != -1)
+			this.remove0(idx);
+		return idx != -1;
+	}
+
+	@Override
+	public boolean containsAll(@Nonnull final Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean addAll(@Nonnull final Collection<? extends T> c) {
+		boolean result = false;
+		for (final T element : c)
+			result = result || this.add(element);
+		return result;
+	}
+
+	@Override
+	public boolean removeAll(@Nonnull final Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		this.data = new Object[this.data.length];
+		this.insertionIdx = 0;
 	}
 
 }
