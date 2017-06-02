91a9359fccc18d31eb638acba688625eb5d0f0e0
OreCruncher <tedjkaiser@gmail.com>
2017-06-02 10:33:44 -0700

    Refactor handlers

--- src/main/java/org/blockartistry/DynSurround/client/handlers/FootstepsHandler.java
+++ src/main/java/org/blockartistry/DynSurround/client/handlers/FootstepsHandler.java
@@ -30,11 +30,16 @@ import org.blockartistry.DynSurround.ModOptions;
 import org.blockartistry.DynSurround.api.events.FootstepEvent;
 import org.blockartistry.DynSurround.client.fx.ParticleCollections;
 import org.blockartistry.DynSurround.client.handlers.EnvironStateHandler.EnvironState;
+import org.blockartistry.DynSurround.client.sound.SoundEffect;
+import org.blockartistry.DynSurround.registry.BlockRegistry;
 import org.blockartistry.DynSurround.registry.FootstepsRegistry;
 import org.blockartistry.DynSurround.registry.RegistryManager;
 import org.blockartistry.DynSurround.registry.RegistryManager.RegistryType;
+import org.blockartistry.lib.WorldUtils;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -44,17 +49,28 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class FootstepsHandler extends EffectHandlerBase {
 
-	private FootstepsRegistry footsteps;
+	protected final FootstepsRegistry footsteps;
+	protected final BlockRegistry registry;
 
 	public FootstepsHandler() {
 		super("FootstepsHandler");
 
 		this.footsteps = RegistryManager.get(RegistryType.FOOTSTEPS);
-	}
+		this.registry = RegistryManager.<BlockRegistry>get(RegistryType.BLOCK);
+}
 
 	@Override
 	public void process(@Nonnull final World world, @Nonnull final EntityPlayer player) {
 		this.footsteps.process(world, player);
+		
+		if (EnvironState.isPlayerOnGround() && EnvironState.isPlayerMoving()) {
+			final BlockPos pos = EnvironState.getPlayerPosition().down(1);
+			final IBlockState state = WorldUtils.getBlockState(world, pos);
+			final SoundEffect sound = this.registry.getStepSoundToPlay(state, RANDOM);
+			if (sound != null)
+				sound.doEffect(WorldUtils.getDefaultBlockStateProvider(), state, pos, RANDOM);
+		}
+
 	}
 
 	@SubscribeEvent
