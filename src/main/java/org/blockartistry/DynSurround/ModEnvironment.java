a706676509a1173086799c8f52a9a0e9b89f30fc
OreCruncher <tedjkaiser@gmail.com>
2017-08-12 20:45:19 -0700

    Limit lighting hud to only when albedo is loaded

--- src/main/java/org/blockartistry/DynSurround/ModEnvironment.java
+++ src/main/java/org/blockartistry/DynSurround/ModEnvironment.java
@@ -54,7 +54,10 @@ public enum ModEnvironment {
 	//
 	GalacticraftPlanets("galacticraftplanets"),
 	//
-	CoFHCore("cofhcore");
+	CoFHCore("cofhcore"),
+	//
+	Albedo("albedo");
+
 	
 	protected final String modId;
 	protected boolean isLoaded;
