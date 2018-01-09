fc15c86796ffc21d9711ba2f63ee0cae85238984
OreCruncher <tedjkaiser@gmail.com>
2018-01-09 11:10:14 -0800

    Oops

--- src/main/java/org/blockartistry/DynSurround/registry/BiomeRegistry.java
+++ src/main/java/org/blockartistry/DynSurround/registry/BiomeRegistry.java
@@ -29,7 +29,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import javax.annotation.Nonnull;
