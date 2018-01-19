3fccf3846628d4f2f1789336486db3fa277c86b3
OreCruncher <tedjkaiser@gmail.com>
2018-01-19 08:38:21 -0800

    Footprints as transparent

--- src/main/java/org/blockartistry/DynSurround/client/fx/particle/mote/ParticleCollectionFootprint.java
+++ src/main/java/org/blockartistry/DynSurround/client/fx/particle/mote/ParticleCollectionFootprint.java
@@ -86,6 +86,11 @@ public class ParticleCollectionFootprint extends ParticleCollection {
 
 	}
 
+	@Override
+	public boolean shouldDisableDepth() {
+		return true;
+	}
+
 	protected void bindTexture(@Nonnull final ResourceLocation resource) {
 		final ResourceLocation res = Style.getStyle(ModOptions.footprintStyle).getTexture();
 		super.bindTexture(res);
