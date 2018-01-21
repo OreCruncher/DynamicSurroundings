209b91c95e5cd8aeffc699804679933daf4cd43e
OreCruncher <tedjkaiser@gmail.com>
2018-01-21 11:38:58 -0800

    Rerender texture after load if optifine

--- src/main/java/org/blockartistry/DynSurround/proxy/ProxyClient.java
+++ src/main/java/org/blockartistry/DynSurround/proxy/ProxyClient.java
@@ -56,6 +56,7 @@ import net.minecraft.client.resources.IResourceManagerReloadListener;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -67,12 +68,12 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class ProxyClient extends Proxy implements IResourceManagerReloadListener {
-	
+
 	@Override
 	protected void registerLanguage() {
 		Localization.initialize(Side.CLIENT);
 	}
-	
+
 	@Override
 	protected void eventBusRegistrations() {
 		super.eventBusRegistrations();
@@ -111,13 +112,13 @@ public class ProxyClient extends Proxy implements IResourceManagerReloadListener
 	@Override
 	public void init(@Nonnull final FMLInitializationEvent event) {
 		super.init(event);
-		
+
 		KeyHandler.init();
 		ParticleDripOverride.register();
-		
+
 		ClientCommandHandler.instance.registerCommand(new CommandCalc());
-		
-		if(ModOptions.disableWaterSuspendParticle)
+
+		if (ModOptions.disableWaterSuspendParticle)
 			Minecraft.getMinecraft().effectRenderer.registerParticle(EnumParticleTypes.SUSPENDED.getParticleID(), null);
 	}
 
@@ -134,6 +135,9 @@ public class ProxyClient extends Proxy implements IResourceManagerReloadListener
 	public void clientConnect(@Nonnull final ClientConnectedToServerEvent event) {
 		Minecraft.getMinecraft().addScheduledTask(new Runnable() {
 			public void run() {
+				// Fire it again because of, you know, stupidity
+				if (FMLClientHandler.instance().hasOptifine())
+					onResourceManagerReload(Minecraft.getMinecraft().getResourceManager());
 				EffectManager.register();
 				GuiHUDHandler.register();
 				ProxyClient.this.connectionTime = System.currentTimeMillis();
