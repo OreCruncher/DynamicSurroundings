1645fc9b737d18c3ba8436601dc0553dbad1c8c6
OreCruncher <tedjkaiser@gmail.com>
2018-02-05 16:34:51 -0800

    Code cleanup, isle 3!

--- src/main/java/org/blockartistry/DynSurround/registry/BiomeInfo.java
+++ src/main/java/org/blockartistry/DynSurround/registry/BiomeInfo.java
@@ -167,6 +167,10 @@ public final class BiomeInfo implements Comparable<BiomeInfo> {
 		}
 	}
 
+	public boolean hasWeatherEffect() {
+		return this.getHasPrecipitation() || this.getHasDust();
+	}
+
 	public boolean getHasPrecipitation() {
 		return this.hasPrecipitation;
 	}
