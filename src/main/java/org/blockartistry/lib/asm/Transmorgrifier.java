f72cb9a192e008e77e600826eb11851001ebfc88
OreCruncher <tedjkaiser@gmail.com>
2018-02-15 16:47:08 -0800

    More ASM cleanup

--- src/main/java/org/blockartistry/lib/asm/Transmorgrifier.java
+++ src/main/java/org/blockartistry/lib/asm/Transmorgrifier.java
@@ -25,6 +25,7 @@
 package org.blockartistry.lib.asm;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.ClassWriter;
@@ -32,19 +33,27 @@ import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.LocalVariableNode;
 import org.objectweb.asm.tree.MethodNode;
 
+/**
+ * Handles ASM modification for a class.
+ */
 public abstract class Transmorgrifier {
 
 	private final String className;
 
-	public Transmorgrifier(final String className) {
+	public Transmorgrifier(@Nonnull final String className) {
 		this.className = className;
 	}
 
+	/**
+	 * ClassWriter flags to use when processing the byte code.
+	 * 
+	 * @return Set of flags to use
+	 */
 	public int classWriterFlags() {
 		return ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES;
 	}
 
-	/*
+	/**
 	 * Name of the Transmorgrifier. Used in logging to identify which set of logic
 	 * was operating.
 	 */
@@ -61,7 +70,7 @@ public abstract class Transmorgrifier {
 		return this.className;
 	}
 
-	/*
+	/**
 	 * Indicates whether the transmorgrification process should executed. Override
 	 * to provide custom behavior, such as configuration control.
 	 */
@@ -69,44 +78,101 @@ public abstract class Transmorgrifier {
 		return true;
 	}
 
-	public boolean matches(final String name) {
+	/**
+	 * Used by the Transformer to determine if a class that is being processed
+	 * matches what the Transmorgifier is looking for.
+	 * 
+	 * @param name
+	 *            Name of the class being processed
+	 * @return true if the Transmorgrifier is interested, false otherwise
+	 */
+	public boolean matches(@Nonnull final String name) {
 		return this.className.equals(name);
 	}
 
-	/*
+	/**
 	 * Override to provide ASM logic.
 	 */
 	public abstract boolean transmorgrify(final ClassNode cn);
 
-	// Helper methods
-	protected MethodNode findMethod(final ClassNode cn, final String name, final String signature) {
+	/**
+	 * Locates a MethodNode within the specified ClassNode.
+	 * 
+	 * @param cn
+	 *            ClassNode to search
+	 * @param signature
+	 *            Signature of the method
+	 * @param name
+	 *            Name of the method
+	 * @return MethodNode that matches the search criteria, null otherwise
+	 */
+	@Nullable
+	protected MethodNode findMethod(@Nonnull final ClassNode cn, @Nonnull final String signature,
+			@Nonnull final String name) {
 		for (final MethodNode m : cn.methods)
 			if (m.name.equals(name) && m.desc.equals(signature))
 				return m;
 		return null;
 	}
 
-	protected MethodNode findMethod(final ClassNode cn, final String[] name, final String[] signature) {
-		for (int i = 0; i < name.length; i++) {
-			final MethodNode m = findMethod(cn, name[i], signature[i]);
+	/**
+	 * Locates a MethodNode within the specified ClassNode. The method accepts one
+	 * or more method names and will return the first one discovered. Useful for
+	 * handling obfuscation.
+	 * 
+	 * @param cn
+	 *            ClassNode to search
+	 * @param signature
+	 *            Signature of the method
+	 * @param names
+	 *            One or more names to search for
+	 * @return MethodNode that matches the search criteria, null otherwise
+	 */
+	@Nullable
+	protected MethodNode findMethod(@Nonnull final ClassNode cn, @Nonnull final String signature,
+			@Nonnull final String... names) {
+		for (int i = 0; i < names.length; i++) {
+			final MethodNode m = findMethod(cn, signature, names[i]);
 			if (m != null)
 				return m;
 		}
 		return null;
 	}
 
-	protected MethodNode findCTOR(final ClassNode cn, final String signature) {
-		return findMethod(cn, "<init>", signature);
+	/**
+	 * Locates a CTOR within the provided ClassNode that has the specified
+	 * signature.
+	 * 
+	 * @param cn
+	 *            ClassNode to search
+	 * @param signature
+	 *            Signature to match
+	 * @return MethodeNode that matches the search criteria, null otherwise
+	 */
+	@Nullable
+	protected MethodNode findCTOR(@Nonnull final ClassNode cn, @Nonnull final String signature) {
+		return findMethod(cn, signature, "<init>");
 	}
 
-	protected LocalVariableNode findLocalVariable(final MethodNode m, final String name) {
+	/**
+	 * Locates a local variable within the MethodNode that matches the specified
+	 * name.
+	 * 
+	 * @param m
+	 *            MethodNode to search
+	 * @param name
+	 *            Local variable to search for
+	 * @return LocalVariableNode that matches the search criteria, null otherwise
+	 */
+	@Nullable
+	protected LocalVariableNode findLocalVariable(@Nonnull final MethodNode m, @Nonnull final String name) {
 		for (final LocalVariableNode v : m.localVariables)
 			if (v.name.equals(name))
 				return v;
 		return null;
 	}
 
-	protected void logMethod(final Logger log, final MethodNode node, final String message) {
+	protected void logMethod(@Nonnull final Logger log, @Nonnull final MethodNode node, @Nonnull final String message) {
 		final String text = String.format("%s%s: %s", node.name, node.desc, message);
 		log.info(text);
 	}
