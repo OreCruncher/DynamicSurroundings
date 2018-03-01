07e08c4dfbefa9e216b7e96115ad84605fac9720
OreCruncher <tedjkaiser@gmail.com>
2018-02-28 20:05:55 -0800

    64 bit seed for LCG

--- src/main/java/org/blockartistry/lib/random/LCGRandom.java
+++ src/main/java/org/blockartistry/lib/random/LCGRandom.java
@@ -29,35 +29,34 @@ package org.blockartistry.lib.random;
  */
 public final class LCGRandom {
 
-	private int v;
+	private long v;
 
 	/**
 	 * Creates and seeds an LCG using an integer from XorShiftRandom.
 	 */
 	public LCGRandom() {
-		this(XorShiftRandom.current().nextInt());
+		this(XorShiftRandom.current().nextLong());
 	}
-	
+
 	/**
 	 * Creates and initializes an LCG generator using a seed value.
 	 * 
 	 * @param seed
 	 *            Seed to initialize the LCG generator with
 	 */
-	public LCGRandom(final int seed) {
+	public LCGRandom(final long seed) {
 		this.v = seed;
 	}
 
 	/**
-	 * Generates a random number between 0 and the bound specified. The bound can be
-	 * at most 32K.
+	 * Generates a random number between 0 and the bound specified.
 	 * 
 	 * @param bound
 	 *            upper bound of the random integer generated
 	 * @return Pseudo random integer between 0 and bound
 	 */
 	public int nextInt(final int bound) {
-		this.v = (214013 * this.v + 2531011);
-		return ((this.v >> 16) & 0x7FFF) % bound;
+		this.v = (2862933555777941757L * this.v + 3037000493L);
+		return ((int) ((this.v >> 32) & 0x7FFFFFFF)) % bound;
 	}
 }
