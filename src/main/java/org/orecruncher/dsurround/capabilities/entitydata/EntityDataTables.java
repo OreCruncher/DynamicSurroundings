c7a9293e52584badb64f20cc47d6fa9e3989d8f4
OreCruncher <tedjkaiser@gmail.com>
2019-10-13 21:40:21 -0700

    Clean up Entity Data

--- src/main/java/org/orecruncher/dsurround/capabilities/entitydata/EntityDataTables.java
+++ src/main/java/org/orecruncher/dsurround/capabilities/entitydata/EntityDataTables.java
@@ -24,15 +24,15 @@
 
 package org.orecruncher.dsurround.capabilities.entitydata;
 
-import java.util.Set;
-import java.util.stream.Stream;
-
+import java.util.Map;
+import java.util.Map.Entry;
 import javax.annotation.Nonnull;
 
 import org.orecruncher.dsurround.ModBase;
 import org.orecruncher.dsurround.capabilities.CapabilityEntityData;
 
-import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.ai.EntityAIAttackMelee;
 import net.minecraft.entity.ai.EntityAIAttackRanged;
@@ -43,94 +43,144 @@ import net.minecraft.entity.ai.EntityAICreeperSwell;
 import net.minecraft.entity.ai.EntityAIFleeSun;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILeapAtTarget;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAIOcelotAttack;
 import net.minecraft.entity.ai.EntityAIOwnerHurtByTarget;
 import net.minecraft.entity.ai.EntityAIPanic;
 import net.minecraft.entity.ai.EntityAIRunAroundLikeCrazy;
+import net.minecraft.entity.ai.EntityAITasks.EntityAITaskEntry;
 import net.minecraft.entity.ai.EntityAIZombieAttack;
+import net.minecraft.entity.monster.EntityBlaze;
 import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.monster.EntityEvoker;
 import net.minecraft.entity.monster.EntityGhast;
 import net.minecraft.entity.monster.EntityGuardian;
 import net.minecraft.entity.monster.EntityPolarBear;
 import net.minecraft.entity.monster.EntityShulker;
 import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.entity.monster.EntitySpellcasterIllager;
 import net.minecraft.entity.monster.EntitySpider;
+import net.minecraft.entity.monster.EntityVex;
+import net.minecraft.entity.monster.EntityVindicator;
 import net.minecraft.entity.passive.EntityRabbit;
+import net.minecraft.entity.passive.EntityWolf;
 
 public final class EntityDataTables {
 
-	private final static Set<Class<? extends EntityAIBase>> ATTACK_CLASSES = new ReferenceOpenHashSet<>();
-	private final static Set<Class<? extends EntityAIBase>> FLEE_CLASSES = new ReferenceOpenHashSet<>();
-
-	public static void add(@Nonnull final Class<? extends EntityAIBase> clazz, final boolean isAttack) {
-		final Set<Class<? extends EntityAIBase>> theSet = isAttack ? ATTACK_CLASSES : FLEE_CLASSES;
-		if (!theSet.contains(clazz))
-			theSet.add(clazz);
+	private enum ClassType {
+		Neither, Attack, Flee
 	}
 
-	@SuppressWarnings({ "unchecked" })
-	private static Class<? extends EntityAIBase> findInternalClass(@Nonnull Class<? extends EntityLiving> clazz,
-			@Nonnull final String className) {
-		final Class<?>[] classes = clazz.getDeclaredClasses();
-		for (final Class<?> c : classes) {
-			if (c.getName().endsWith(className) && EntityAIBase.class.isAssignableFrom(c))
-				return (Class<? extends EntityAIBase>) c;
+	private final static Map<Class<?>, ClassType> AI_TASKS = new Reference2ObjectOpenHashMap<>();
+	private final static Map<String, ClassType> MAPPINGS = new Object2ObjectOpenHashMap<>();
+
+	private static void add(@Nonnull final Class<?> clazz, @Nonnull ClassType ct) {
+		AI_TASKS.put(clazz, ct);
+		MAPPINGS.put(resolveName(clazz), ct);
+	}
+
+	private static String resolveName(@Nonnull final Class<?> clazz) {
+		String n = clazz.getName();
+		int i = n.lastIndexOf('.');
+		return n.substring(i + 1);
+	}
+
+	private static ClassType find(@Nonnull final EntityAIBase aiTask) {
+		String name = resolveName(aiTask.getClass());
+		ClassType ct = MAPPINGS.get(name);
+
+		if (ct == null) {
+			
+			try {
+				for (Entry<Class<?>, ClassType> kvp : AI_TASKS.entrySet()) {
+					if (kvp.getKey().isInstance(aiTask)) {
+						MAPPINGS.put(name, ct = kvp.getValue());
+						return ct;
+					}
+				}
+			} catch(final Exception ex) {
+				// Weird.  Maybe Eclipse J9 - it's whacky
+				ModBase.log().catching(ex);
+			}
+			
+			MAPPINGS.put(name, ct = ClassType.Neither);
 		}
 
-		return null;
+		return ct;
 	}
 
-	private static void registerSpecial(@Nonnull final Class<? extends EntityLiving> clazz,
-			@Nonnull final String className, final boolean isAttack) {
-		final Class<? extends EntityAIBase> ai = findInternalClass(clazz, className);
-		if (ai != null)
-			add(ai, isAttack);
-		else
-			ModBase.log().warn("Unable to locate class '%s' inside [%s]", className, clazz.toGenericString());
+	private static void add(@Nonnull final Class<? extends EntityLiving> clazz, @Nonnull final String className,
+			final ClassType ct) {
+		String name = resolveName(clazz) + "$" + className;
+		MAPPINGS.put(name, ct);
 	}
 
 	static {
-		add(EntityAIAttackMelee.class, true);
-		add(EntityAIAttackRanged.class, true);
-		add(EntityAIAttackRangedBow.class, true);
-		add(EntityAIAvoidEntity.class, false);
-		add(EntityAICreeperSwell.class, true);
-		add(EntityAIFleeSun.class, false);
-		add(EntityAIHurtByTarget.class, false);
-		add(EntityAILeapAtTarget.class, true);
-		add(EntityAIOcelotAttack.class, true);
-		add(EntityAIOwnerHurtByTarget.class, true);
-		add(EntityAIPanic.class, false);
-		add(EntityAIRunAroundLikeCrazy.class, false);
-		add(EntityAIZombieAttack.class, true);
+		add(EntityAIAttackMelee.class, ClassType.Attack);
+		add(EntityAIAttackRanged.class, ClassType.Attack);
+		add(EntityAIAttackRangedBow.class, ClassType.Attack);
+		add(EntityAICreeperSwell.class, ClassType.Attack);
+		add(EntityAILeapAtTarget.class, ClassType.Attack);
+		add(EntityAIOcelotAttack.class, ClassType.Attack);
+		add(EntityAIOwnerHurtByTarget.class, ClassType.Attack);
+		add(EntityAIZombieAttack.class, ClassType.Attack);
+		add(EntityAINearestAttackableTarget.class, ClassType.Attack);
 
-		// Special embedded AI tasks
-		registerSpecial(EntityEnderman.class, "AIFindPlayer", true);
-		registerSpecial(EntityGhast.class, "AIFireballAttack", true);
-		registerSpecial(EntityGuardian.class, "AIGuardianAttack", true);
-		registerSpecial(EntityPolarBear.class, "AIMeleeAttack", true);
-		registerSpecial(EntityPolarBear.class, "AIPanic", false);
-		registerSpecial(EntityPolarBear.class, "AIAttackPlayer", true);
-		registerSpecial(EntityShulker.class, "AIAttackNearest", true);
-		registerSpecial(EntitySlime.class, "AISlimeAttack", true);
-		registerSpecial(EntitySpider.class, "AISpiderAttack", true);
-		registerSpecial(EntityRabbit.class, "AIAvoidEntity", false);
+		add(EntityAIAvoidEntity.class, ClassType.Flee);
+		add(EntityAIFleeSun.class, ClassType.Flee);
+		add(EntityAIHurtByTarget.class, ClassType.Flee);
+		add(EntityAIPanic.class, ClassType.Flee);
+		add(EntityAIRunAroundLikeCrazy.class, ClassType.Flee);
+
+		// Specials because they are inner classes
+
+		add(EntityEnderman.class, "AIFindPlayer", ClassType.Attack);
+		add(EntityGhast.class, "AIFireballAttack", ClassType.Attack);
+		add(EntityGuardian.class, "AIGuardianAttack", ClassType.Attack);
+		add(EntityPolarBear.class, "AIMeleeAttack", ClassType.Attack);
+		add(EntityPolarBear.class, "AIAttackPlayer", ClassType.Attack);
+		add(EntityShulker.class, "AIAttackNearest", ClassType.Attack);
+		add(EntitySlime.class, "AISlimeAttack", ClassType.Attack);
+		add(EntitySpider.class, "AISpiderAttack", ClassType.Attack);
+		add(EntityBlaze.class, "AIFireballAttack", ClassType.Attack);
+		add(EntityEvoker.class, "AICastingSpell", ClassType.Attack);
+		add(EntityEvoker.class, "AISummonSpell", ClassType.Attack);
+		add(EntityEvoker.class, "AIAttackSpell", ClassType.Attack);
+		add(EntityEvoker.class, "AIWololoSpell", ClassType.Attack);
+		add(EntitySpellcasterIllager.class, "AICastingApell", ClassType.Attack);
+		add(EntitySpellcasterIllager.class, "AIMirriorSpell", ClassType.Attack);
+		add(EntitySpellcasterIllager.class, "AIBlindnessSpell", ClassType.Attack);
+		add(EntityShulker.class, "AIAttack", ClassType.Attack);
+		add(EntityVex.class, "AIChargeAttack", ClassType.Attack);
+		add(EntityVindicator.class, "AIJohnnyAttack", ClassType.Attack);
+
+		add(EntityPolarBear.class, "AIPanic", ClassType.Flee);
+		add(EntityRabbit.class, "AIAvoidEntity", ClassType.Flee);
+		add(EntityWolf.class, "AIAvoidEntity", ClassType.Flee);
 	}
 
 	@Nonnull
-	private static boolean eval(@Nonnull final EntityLiving entity,
-			@Nonnull final Set<Class<? extends EntityAIBase>> markers) {
-		return Stream.of(entity.tasks.executingTaskEntries, entity.targetTasks.executingTaskEntries)
-				.flatMap(e -> e.stream()).map(t -> t.action.getClass()).filter(c -> markers.contains(c)).findFirst()
-				.isPresent();
+	private static boolean eval(@Nonnull final EntityLiving entity, @Nonnull ClassType desiredType) {
+
+		for (EntityAITaskEntry task : entity.tasks.executingTaskEntries) {
+			if (find(task.action) == desiredType)
+				return true;
+		}
+
+		for (EntityAITaskEntry task : entity.targetTasks.executingTaskEntries) {
+			if (find(task.action) == desiredType)
+				return true;
+		}
+
+		return false;
 	}
 
 	@Nonnull
 	public static void assess(@Nonnull final EntityLiving entity) {
 		final IEntityDataSettable data = (IEntityDataSettable) CapabilityEntityData.getCapability(entity);
 		if (data != null) {
-			final boolean isAttacking = eval(entity, ATTACK_CLASSES);
-			final boolean isFleeing = eval(entity, FLEE_CLASSES);
+			final boolean isAttacking = eval(entity, ClassType.Attack);
+			final boolean isFleeing = eval(entity, ClassType.Flee);
 			data.setAttacking(isAttacking);
 			data.setFleeing(isFleeing);
 		}
